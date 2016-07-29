41a9e2fd30ecaa8f5551e1f13c3f16f404d94deb
Brett Uglow <buglow@odecee.com.au>
2016-07-29 12:23:03 +1000

    docs(formInput): adjust line comment to make more sense
    feature/inputs-add-input-group-btn

--- src/modules/ngFormLib/controls/formDate/FormDate.js
+++ src/modules/ngFormLib/controls/formDate/FormDate.js
@@ -32,7 +32,7 @@ mod.directive('formDate', ['formControlService', function(formControlService) {
       formControlService.addLabelText(labelElem, tAttr.label);
       addPlaceholder(inputElem, formControlService.translate(tAttr.placeholder)); // Do this to be API-compatible with the form-select control. ff-placeholder is still supported. Use one or the other.
 
-      // If the user wants to use 'input-addon-*' or 'input-addon-*', change the DOM
+      // If the user wants to use addons (either text or buttons), change the DOM
       var hasInputGroup = formControlService.addInputGroup(inputElem, tAttr);
       var parentElemForErrors = (hasInputGroup) ? inputElem.parent().parent() : inputElem.parent();
 
